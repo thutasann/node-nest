198ac01d68a1c853c5f7b13ad37dcc059bb04584
thutasann <thutasann2002@gmail.com>
2024-06-30 01:26:50 +0800

    feat: handle error with logger

--- app-backend/catalog_service/src/app.ts
+++ app-backend/catalog_service/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import setupSwagger from './swagger';
 import catalogRouter from './routes/catalog.route';
 import { httpLogger } from './utils/logger';
+import { HandleErrorWithLogger } from './utils/error/handler';
 
 const app = express();
 
@@ -11,7 +12,7 @@ setupSwagger(app);
 app.use(express.json());
 app.use(httpLogger);
 
-//TODO:handle_error_with_logger
+app.use(HandleErrorWithLogger);
 
 // routes
 app.use('/api', catalogRouter);
--- app-backend/catalog_service/src/utils/error/handler.ts
+++ app-backend/catalog_service/src/utils/error/handler.ts
@@ -1,8 +1,47 @@
 import type { Request, Response, NextFunction } from 'express';
+import { AuthorizeError, NotFoundError, ValidationError } from './errors';
+import { logger } from '../logger';
 
+/**
+ * Handle Error With Logger
+ * @param error - error types
+ * @param res - response
+ * @returns
+ */
 export const HandleErrorWithLogger = (
 	error: Error,
 	req: Request,
 	res: Response,
 	next: NextFunction,
-) => {};
+) => {
+	/** error report from tools implementations eg: Cloudwatch, Sentry etc */
+	let reportError = true;
+	let status = 500;
+	let data = error.message;
+
+	// skip common / known errors
+	[NotFoundError, ValidationError, AuthorizeError].forEach((errorType) => {
+		if (error instanceof errorType) {
+			reportError = false;
+			status = error.status;
+			data = error.message;
+		}
+	});
+
+	if (reportError) {
+		logger.error(error);
+	} else {
+		logger.warn(error);
+	}
+
+	return res.status(status).json(data);
+};
+
+/**
+ * Hanlde UnCaught Exception
+ * @param error - UncaughtException
+ */
+export const HandleUnCaughtException = async (error: Error) => {
+	logger.error(error);
+	process.exit(1);
+};
