4acba4f3a5220509949ee7a52cc510bfc76f7aee
thutasann <thutasann2002@gmail.com>
2024-07-06 00:17:37 +0800

    feat: domain module init

--- nest-advanced/apps/01-nestjs-baseline-rest-apis/src/app.module.ts
+++ nest-advanced/apps/01-nestjs-baseline-rest-apis/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DomainModoule } from './app/domain/domain.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [DomainModoule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
---
+++ nest-advanced/apps/01-nestjs-baseline-rest-apis/src/app/domain/domain.module.ts
@@ -0,0 +1,13 @@
+import { DBModule } from '@dev/database';
+import { Module } from '@nestjs/common';
+
+@Module({
+	imports: [
+		// DBModule.forRoot({
+		// 	entities: [],
+		// }),
+	],
+	providers: [],
+	controllers: [],
+})
+export class DomainModoule {}
