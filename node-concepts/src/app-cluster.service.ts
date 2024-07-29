4fb4ebcf8aa3e2d6373b983096c79c36fa32255a
thutasann <thutasann2002@gmail.com>
2024-07-30 00:48:07 +0800

    docs: cluster note

--- node-concepts/src/app-cluster.service.ts
+++ node-concepts/src/app-cluster.service.ts
@@ -5,6 +5,18 @@ import { Injectable } from '@nestjs/common';
 
 const numCPUs = os.cpus().length;
 
+/**
+ * App Cluster Service
+ * @description
+ * - the cluster module allows nodejs to spawn multiple processes
+ * - that can share the same server port and handle the incoming requests
+ * - each processes runs on a separate CPU core
+ * @example
+ * - high availability of services
+ * - little to no downtime
+ * - basic scalability based on demand
+ * @link https://www.youtube.com/watch?v=bRZTvCwcp20&list=PL5Lsd0YA4OMGN86vWiW7O52izu-cTxcS3&index=4
+ */
 @Injectable()
 export class AppClusterService {
 	static clusterize(callback: Function): void {
