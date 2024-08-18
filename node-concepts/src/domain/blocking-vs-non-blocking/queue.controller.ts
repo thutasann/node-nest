e9825308d819b3704c156d10fd181120f71b98c9
thutasann <thutasann2002@gmail.com>
2024-08-18 15:05:20 +0800

    add: timer queue

--- node-concepts/src/domain/blocking-vs-non-blocking/queue.controller.ts
+++ node-concepts/src/domain/blocking-vs-non-blocking/queue.controller.ts
@@ -73,6 +73,30 @@ export class Queuecontroller {
 		})();
 	}
 
+	/**
+	 * Queue result [process -> promise -> timer]
+	 * @description
+	 * - timer queue callbacks are executed in FIFO order
+	 */
+	@Get('/timer-queue')
+	timerQueueSample() {
+		setTimeout(() => console.log('this is settimeout 1'), 0);
+		setTimeout(() => {
+			console.log('this is settimeout 2');
+			// callbacks in microtask queues are executed in between the execution of callbacks in the timer queue
+			process.nextTick(() =>
+				console.log('this is the inner next tick inside setTimeout'),
+			);
+		}, 0);
+		setTimeout(() => console.log('this is settimeout 3'), 0);
+
+		process.nextTick(() => console.log('this is process.nextTick 1'));
+		process.nextTick(() => console.log('this is process.nextTick 2'));
+
+		Promise.resolve().then(() => console.log('Promise resolved 1'));
+		Promise.resolve().then(() => console.log('Promise resolved 2'));
+	}
+
 	@Get('/settimeout-sample')
 	setTimeoutSample() {
 		setTimeout(() => {
--- node-concepts/workers/playground.js
+++ node-concepts/workers/playground.js
@@ -1,4 +1,19 @@
 // ---------------- playground file ----------------
 
-Promise.resolve().then(() => console.log('this is promice.resolve 1'));
+// Promise.resolve().then(() => console.log('this is promice.resolve 1'));
+// process.nextTick(() => console.log('this is process.nextTick 1'));
+
+setTimeout(() => console.log('this is settimeout 1'), 0);
+setTimeout(() => {
+	console.log('this is settimeout 2');
+	process.nextTick(() =>
+		console.log('this is the inner next tick inside setTimeout'),
+	);
+}, 0);
+setTimeout(() => console.log('this is settimeout 3'), 0);
+
 process.nextTick(() => console.log('this is process.nextTick 1'));
+process.nextTick(() => console.log('this is process.nextTick 2'));
+
+Promise.resolve().then(() => console.log('Promise resolved 1'));
+Promise.resolve().then(() => console.log('Promise resolved 2'));
