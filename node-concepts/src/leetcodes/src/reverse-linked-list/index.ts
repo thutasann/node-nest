b03227caaa8d69bc0b869bf8acf1aaafda240afd
thutasann <thutasann2002@gmail.com>
2024-09-08 13:29:04 +0800

    chore: folder strcut change

--- node-concepts/src/leetcodes/src/reverse-linked-list/index.ts
+++ node-concepts/src/leetcodes/src/reverse-linked-list/index.ts
@@ -1,4 +1,18 @@
+/**
+ * Singly Linked List
+ */
+class ListNode {
+	val: number;
+	next: ListNode | null;
+
+	constructor(val?: number, next?: ListNode | null) {
+		this.val = val || 0;
+		this.next = next || null;
+	}
+}
+
+/** Reverse Linked List */
 export abstract class ReverseLinkedList {
 	/** Reverse Linked List */
-	public static solutionOne() {}
+	public static solutionOne(head: ListNode | null) {}
 }
